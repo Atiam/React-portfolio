5c318d572c1dd8de6b15147d90680c839ad75fa2
Arnaud Tiam <arnaudtiam50@gmail.com>
2024-08-01 12:17:50 -0500

    Update code.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,7 +2,8 @@ export default function Resume(){
     return (
       <main>
         <h2>Resume</h2>
-        <a href="../src/assets/RESUME-ARNAUD_TIAM.pdf">Click here to view my resume</a>
+        {/* <a href="../src/assets/RESUME-ARNAUD_TIAM.pdf">Click here to view my resume</a> */}
+        <a href="https://github.com/Atiam/React-portfolio/blob/main/src/assets/RESUME-ARNAUD_TIAM.pdf">Click here to view my resume</a>
       </main>
     );
 }
